3143edbc22e70cc1e9c7bcd2e480766d7dde1f1c
Roberto Ramirez <robert.624@hotmail.com>
2023-04-01 01:37:01 -0500

    added error styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from "react";
 import "./App.scss";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [emailError, setEmailError] = useState(false);
+
+    function handleSubmit (e) {
+
+        e.preventDefault();
+        const rawEmail = e.target[0].value;
+
+        // regex for email validation 
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+        if (emailRegex.test(rawEmail) === false) {
+            setEmailError(true);
+        }else {
+            setEmailError(false);
+            console.log("email is valid, sending...")
+        }
+    }
 
     return (
         <div className="App">
@@ -18,38 +34,41 @@ function App() {
                     <div className="logo-desktop-container">
                         <img src="/logo.svg" alt="desktop logo" className="desktop-logo"/>
                     </div>
-                  <div className="main-text">
-                    <h1>We're <strong>coming soon</strong></h1>
-                    <p>
-                        {" "}
-                        Hello fellow shoppers! We're currently building our new
-                        fashion store. Add your email below to stay up-to-date with
-                        announcements and our launch deals.
-                    </p>
-                    <form action="POST">
-                        <div className="input-container">
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Email Address"
-                            />
-                            <span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="12"
-                                    height="20"
-                                >
-                                    <path
-                                        fill="none"
-                                        stroke="#FFF"
-                                        stroke-width="2"
-                                        d="M1 1l8.836 8.836L1 18.671"
-                                    />
-                                </svg>
-                            </span>
-                        </div>
-                    </form>
-                  </div>
+                    <div className="main-text">
+                        <h1>We're <strong>coming soon</strong></h1>
+                        <p>
+                            {" "}
+                            Hello fellow shoppers! We're currently building our new
+                            fashion store. Add your email below to stay up-to-date with
+                            announcements and our launch deals.
+                        </p>
+                        <form action="POST" onSubmit={handleSubmit}>
+                            <div className="input-container">
+                                <input
+                                    className={`${emailError ? "email-error" : ""}`}
+                                    type="text"
+                                    name="email"
+                                    placeholder="Email Address"
+                                />
+                                {   emailError && (<img src="/icon-error.svg" className="error-icon"/>)}
+                                <button>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="12"
+                                        height="20"
+                                    >
+                                        <path
+                                            fill="none"
+                                            stroke="#FFF"
+                                            strokeWidth="2"
+                                            d="M1 1l8.836 8.836L1 18.671"
+                                        />
+                                    </svg>
+                                </button>
+                                {   emailError && (<div className="input-error">Please provide a valid email</div>)}
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </main>
         </div>
